c7624597843208fb70e9a7703376d63148ed028a
Michel Dufresne <3496090+cpp-cat@users.noreply.github.com>
2022-03-17 19:14:03 -0400

    Add ability to generate resource from rules to support migration #57

--- jets/compiler/jetrule_validation_contex.py
+++ jets/compiler/jetrule_validation_contex.py
@@ -1,7 +1,52 @@
 from typing import Dict
+from frozendict import frozendict
 from jetrule_context import JetRuleContext
 from absl import flags
 
+RC_OP_MAPPING = frozendict({
+  '!=':                    '!=',
+  '+':                     '+',
+  '-':                     '-',
+  '/':                     '/',
+  '<':                     '<',
+  '<=':                    '<=',
+  '==':                    '==',
+  '>':                     '>',
+  '>=':                    '>=',
+  'abs':                   'abs',
+  'and':                   'and',
+  'apply_format':          'apply_format',
+  'cast_to_range_of':      'to_type_of',
+  'contains':              'contains',
+  'create_entity':         'create_entity',
+  'create_literal':        'create_literal',
+  'create_resource':       'create_resource',
+  'create_uuid_resource':  'create_uuid_resource',
+  'different_from':        '!=',
+  'exist':                 'exist',
+  'exist_not':             'exist_not',
+  'get_cardinality':       'size_of',
+  'is_literal':            'is_literal',
+  'is_no_value':           'is_null',
+  'length_of':             'length_of',
+  'literal_regex':         'literal_regex',
+  'lookup':                'lookup',
+  'max_multi_value':       'max_of',
+  'min_multi_value':       'min_of',
+  'multi_lookup':          'multi_lookup',
+  'not':                   'not',
+  'or':                    'or',
+  'parse_usd_currency':    'parse_usd_currency',
+  'sorted_head':           'sorted_head',
+  'starts_with':           'starts_with',
+  'sum_values':            'sum_values',
+  'to_int':                'to_int',
+  'to_real':               'to_real',
+  'to_upper':              'to_upper',
+
+
+
+})
 class ValidationContext:
   def __init__(self, jetrule_ctx: JetRuleContext):
     self.jetrule_ctx = jetrule_ctx
