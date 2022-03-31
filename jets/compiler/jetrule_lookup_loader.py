6126a43f3852acbabfea9da4518749b4f9f87843
JMDierkes <11888503+JMDierkes@users.noreply.github.com>
2022-03-31 10:07:23 -0400

    Added boolean converter. Removed apsw dependency. Added more typing.

--- jets/compiler/jetrule_lookup_loader.py
+++ jets/compiler/jetrule_lookup_loader.py
@@ -6,7 +6,6 @@ from jetrule_lookup_sqlite import JetRuleLookupSQLite
 
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string("lookup_path", None, "Path to Lookup Directory relative to base_path")
 flags.DEFINE_string("base_path", None, "Base path for lookup_path, out_file and all imported files")
 
 
@@ -16,11 +15,7 @@ flags.DEFINE_string("base_path", None, "Base path for lookup_path, out_file and 
 def main(argv):
   del argv  # Unused.
 
-  
-  in_fname = Path(FLAGS.lookup_path)
-
   base_path = Path(FLAGS.base_path)
-
 
   lookup_db_helper = JetRuleLookupSQLite()
   err = lookup_db_helper.saveLookups()
@@ -29,5 +24,5 @@ def main(argv):
     sys.exit('ERROR while saving Lookup file to lookup_db: {0}.'.format(str(err))) 
 
 if __name__ == '__main__':
-  flags.mark_flag_as_required('lookup_path')
+  flags.mark_flag_as_required('base_path')
   app.run(main)
