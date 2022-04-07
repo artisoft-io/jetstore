56dcd9c602bf5c2ff12730494f4c0c8d319567fe
JMDierkes <11888503+JMDierkes@users.noreply.github.com>
2022-04-07 14:37:25 -0400

    Added append functionality to lookup loader

--- jets/compiler/jetrule_lookup_loader.py
+++ jets/compiler/jetrule_lookup_loader.py
@@ -7,7 +7,9 @@ from jetrule_lookup_sqlite import JetRuleLookupSQLite
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string("base_path", None, "Base path for lookup_path, out_file and all imported files")
-
+flags.DEFINE_string("lookup_db", 'jetrule_lookup.db', "JetRule lookup")
+flags.DEFINE_string("rete_db", 'jetrule_rete.db', "JetRule rete config")
+flags.DEFINE_bool("append_db", False, "Append new tables to JetRule lookup db if already exists, already existing tables will be dropped and recreated if provided again by rete_db", short_name='a')
 
 # =======================================================================================
 # command line invocation
@@ -16,9 +18,12 @@ def main(argv):
   del argv  # Unused.
 
   base_path = Path(FLAGS.base_path)
+  lookup_db = FLAGS.lookup_db
+  rete_db   = FLAGS.rete_db
+  append_db = FLAGS.append_db
 
-  lookup_db_helper = JetRuleLookupSQLite(base_path=base_path)
-  err = lookup_db_helper.saveLookups()
+  lookup_db_helper = JetRuleLookupSQLite(base_path)
+  err = lookup_db_helper.saveLookups(lookup_db, rete_db, append_db)
   if err:
     print('ERROR while saving Lookup file to lookup_db: {0}.'.format(str(err)))
     sys.exit('ERROR while saving Lookup file to lookup_db: {0}.'.format(str(err))) 
