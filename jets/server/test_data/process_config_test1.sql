178b62da7409afd03d843b2bada0b3cf5a156b76
Michel Dufresne <3496090+cpp-cat@users.noreply.github.com>
2022-05-02 11:06:37 -0400

    Updated server unit tests

--- jets/server/test_data/process_config_test1.sql
+++ jets/server/test_data/process_config_test1.sql
@@ -1,12 +1,14 @@
 TRUNCATE TABLE process_config, process_input, process_mapping, rule_config, process_merge;
 
 INSERT INTO process_config (key, client, description, main_entity_rdf_type) VALUES
-  (1, 'ACME', 'Test ACME file', 'hc:Claim'),
+  (1, 'ACME', 'Lookup ACME Service Code with Modifier', 'hc:Claim'),
+  (3, 'ACME', 'Lookup ACME Service Code without Modifier', 'hc:Claim'),
   (2, 'M2C', 'Test M2C file', 'm2c:Claim')
 RETURNING key;
 
 INSERT INTO process_input (key, process_key, input_table, entity_rdf_type, grouping_column, key_column) VALUES
-  (1, 1, 'test1', 'hc:Claim', 'MEMBER_NUMBER', 'CLAIM_NUMBER'),
+  (1, 1, 'test_table', 'hc:Claim', 'MEMBER_NUMBER', 'CLAIM_NUMBER'),
+  (3, 3, 'test_table', 'hc:Claim', 'MEMBER_NUMBER', 'CLAIM_NUMBER'),
   (2, 2, 'm2c__input1', 'm2c:Claim', 'COL2', NULL)
 ;
 
@@ -16,6 +18,11 @@ INSERT INTO process_mapping (process_input_key, input_column, data_property, fun
   (1, 'DOS', 'hc:date_of_service', NULL, NULL, NULL),
   (1, 'SERVICE_CODE', 'hc:service_code', NULL, NULL, NULL),
   (1, 'MODIFIER', 'hc:modifier', NULL, NULL, NULL),
+  (3, 'MEMBER_NUMBER', 'hc:member_number', NULL, NULL, NULL),
+  (3, 'CLAIM_NUMBER', 'hc:claim_number', NULL, NULL, NULL),
+  (3, 'DOS', '_0:date_of_service', NULL, NULL, NULL),
+  (3, 'SERVICE_CODE', 'hc:service_code', NULL, NULL, NULL),
+  (3, 'MODIFIER', 'hc:modifier', NULL, NULL, NULL),
   (2, 'COL1', 'm2c:P1', 'regex', '\d{3}', NULL),
   (2, 'COL2', 'm2c:P2', NULL, NULL, NULL),
   (2, 'COL2', 'm2c:P3', 'parse_amount', '100', '0')
@@ -23,6 +30,7 @@ INSERT INTO process_mapping (process_input_key, input_column, data_property, fun
 
 INSERT INTO rule_config (process_key, subject, predicate, object, rdf_type) VALUES
   (1, 'jets:iState', 'lk:withModifier', 'true', 'bool'),
+  (3, 'jets:iState', 'lk:withModifier', 'false', 'bool'),
   (2, 'subject2', 'predicate1', 'object1', 'int'),
   (2, 'subject2', 'predicate2', 'object2', 'date')
 ;
