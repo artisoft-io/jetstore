4329f11125f72f5cc3a5bf03b0860361df228406
Michel Dufresne <3496090+cpp-cat@users.noreply.github.com>
2022-05-24 09:51:36 -0400

    reverting an unproven improvement

--- jets/rete/rete_session.cc
+++ jets/rete/rete_session.cc
@@ -254,9 +254,9 @@ namespace jets::rete {
 
         // mark current beta node as activated and push it on the stack so to visit it's childrens
         if(need_all_rows) current_relation->set_activated(true);
-        if(current_relation->has_pending_rows()) {
+        // if(current_relation->has_pending_rows()) {
           stack.push_back(current_vertex);
-        }
+        // }
       }
       // // Clear the pending rows of parent node since we propagated to all it's children
       // parent_beta_relation->clear_pending_rows();
