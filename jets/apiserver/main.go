bd71fcf9eb4b972725b393434cffc70169888aa7
Michel Dufresne <3496090+cpp-cat@users.noreply.github.com>
2022-06-14 00:15:03 -0400

    Initial login JetStore Client User Login #296

--- jets/apiserver/main.go
+++ jets/apiserver/main.go
@@ -1,118 +1,16 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
-
-	"github.com/gorilla/mux"
-	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 var apiSecret          = flag.String("API_SECRET", "", "Secret used for signing jwt tokens (required)")
 var dropTable          = flag.Bool  ("d", false, "drop users table if it exists, default is false")
 var dsn                = flag.String("dsn", "", "primary database connection string (required)")
 var serverAddr         = flag.String("serverAddr", ":8080", "server address to ListenAndServe (required)")
-
-type Server struct {
-	dbpool *pgxpool.Pool
-	Router *mux.Router
-}
-var server = Server{}
-
-// Middleware Function for json header
-func jsonh(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		next(w, r)
-	}
-}
-// Middleware Function for validating jwt token
-func authh(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		err := TokenValid(r)
-		if err != nil {
-			ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
-			return
-		}
-		next(w, r)
-	}
-}
-// Middleware Function for allowing selected cors client
-func corsh(next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("*** Origin Header:", r.Header.Get("Origin"))
-		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
-		next(w, r)
-	}
-}
-
-// Options ------------------------------------------------------------
-func options(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Options Called:", r.URL, "method:",r.Method)
-	for k,v := range r.Header {
-		fmt.Println("    header:",k,"values:",v)
-	}
-	// write cors headers
-	//* check that origin is what we expect
-	w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-	//
-	w.WriteHeader(http.StatusOK)
-}
-
-// processFile
-// --------------------------------------------------------------------------------------
-func listenAndServe() error {
-	// Open db connection
-	var err error
-	server.dbpool, err = pgxpool.Connect(context.Background(), *dsn)
-	if err != nil {
-		return fmt.Errorf("while opening db connection: %v", err)
-	}
-	defer server.dbpool.Close()	
-
-	// prepare users table
-	err = updateUserSchema(server.dbpool, *dropTable)
-	if err != nil {
-		return fmt.Errorf("while updating users table: %v", err)
-	}
-
-	// setup the http routes
-	server.Router = mux.NewRouter()
-	// server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
-
-	// Set Routes
-	// Home Route
-	server.Router.HandleFunc("/", jsonh(server.Home)).Methods("GET")
-
-	// Login Route
-	server.Router.HandleFunc("/login", jsonh(corsh(server.Login))).Methods("POST")
-	server.Router.HandleFunc("/login", options).Methods("OPTIONS")
-
-	//Users routes
-	server.Router.HandleFunc("/register", jsonh(server.CreateUser)).Methods("POST")
-	server.Router.HandleFunc("/users", jsonh(authh(server.GetUsers))).Methods("GET")
-	server.Router.HandleFunc("/users/info", jsonh(authh(server.GetUserDetails))).Methods("GET")
-	server.Router.HandleFunc("/users/{id}", jsonh(authh(server.GetUser))).Methods("GET")
-	server.Router.HandleFunc("/users/{id}", jsonh(authh(server.UpdateUser))).Methods("PUT")
-	server.Router.HandleFunc("/users/{id}", authh(server.DeleteUser)).Methods("DELETE")
-
-	// //Posts routes
-	// server.Router.HandleFunc("/posts", jsonh(server.CreatePost)).Methods("POST")
-	// server.Router.HandleFunc("/posts", jsonh(server.GetPosts)).Methods("GET")
-	// server.Router.HandleFunc("/posts/{id}", jsonh(server.GetPost)).Methods("GET")
-	// server.Router.HandleFunc("/posts/{id}", jsonh(authh(server.UpdatePost))).Methods("PUT")
-	// server.Router.HandleFunc("/posts/{id}", authh(server.DeletePost)).Methods("DELETE")
-
-	fmt.Println("Listening to address ",*serverAddr)
-	return http.ListenAndServe(*serverAddr, server.Router)
-}
 
 func main() {
 	flag.Parse()
---
+++ jetsclient/lib/models/user.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'user.g.dart';
+
+/// The [UserModel] represent the logged-in user
+@JsonSerializable()
+class UserModel {
+  /// The user name obtained from the token
+  String? name;
+
+  /// The login email
+  String? email;
+
+  /// The jwt token (obtained by login form)
+  String? token;
+
+  /// The user's password (used in login form)
+  String? password;
+
+  UserModel({this.name, this.email, this.token, this.password});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UserModelToJson(this);
+}
