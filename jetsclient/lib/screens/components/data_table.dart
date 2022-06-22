9481858b79ff9979d3cfb2fbc781a58684bee53f
Michel Dufresne <3496090+cpp-cat@users.noreply.github.com>
2022-06-22 10:44:12 -0400

    Configured PaginatedDataTable, ned to fix layout still

--- jetsclient/lib/screens/components/data_table.dart
+++ jetsclient/lib/screens/components/data_table.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:jetsclient/http_client.dart';
+
 import 'package:jetsclient/utils/data_table_config.dart';
 import 'package:jetsclient/utils/constants.dart';
+import 'package:provider/provider.dart';
 
 //* examples
 typedef FncBool = void Function(bool?);
@@ -22,18 +25,27 @@ class _JetsDataTableState extends State<JetsDataTableWidget> {
     sortColumnIndex = tableConfig!.sortColumnIndex;
     sortAscending = tableConfig!.sortAscending;
     rowsPerPage = tableConfig!.rowsPerPage;
-    selectedRows = List<bool>.filled(tableConfig!.rowsPerPage, false);
+    dataSource = _JetsDataTableSource(
+        this, Provider.of<HttpClient>(context, listen: false));
+    dataSource!.addListener(() {
+      setState(() {
+        _triggerBuild =
+            _triggerBuild; // this is only to trigger the widget build
+      });
+    });
   }
 
   final ScrollController _verticalController = ScrollController();
   final ScrollController _horizontalController = ScrollController();
-  static const int numItems = 10;
-  var selectedRows = <bool>[];
+  _JetsDataTableSource? dataSource;
   bool isTableEditable = false;
   TableConfig? tableConfig;
   int sortColumnIndex = 0;
   bool sortAscending = false;
   int rowsPerPage = 10;
+  // sentinel variable used only to trigger the widget build
+  // when the dataSource is modified
+  bool _triggerBuild = true;
 
   List<DataColumn> get dataColumns {
     return tableConfig!.columns
@@ -46,13 +58,78 @@ class _JetsDataTableState extends State<JetsDataTableWidget> {
         .toList();
   }
 
+  Widget _makeActions(String actionKey) {
+    switch (actionKey) {
+      case 'newRow':
+        return ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            // Foreground color
+            onPrimary: Theme.of(context).colorScheme.onSecondaryContainer,
+            // Background color
+            primary: Theme.of(context).colorScheme.secondaryContainer,
+          ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
+          onPressed: () => _showDialog('New Row!'),
+          child: const Text('New Row'),
+        );
+      case 'editTable':
+        return ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            // Foreground color
+            onPrimary: Theme.of(context).colorScheme.onSecondaryContainer,
+            // Background color
+            primary: Theme.of(context).colorScheme.secondaryContainer,
+          ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
+          onPressed: () => _showDialog('Edit Table!'),
+          child: const Text('Edit Table'),
+        );
+      case 'saveChanges':
+        return ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            // Foreground color
+            onPrimary: Theme.of(context).colorScheme.onSecondaryContainer,
+            // Background color
+            primary: Theme.of(context).colorScheme.secondaryContainer,
+          ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
+          onPressed: null,
+          child: const Text('Save Changes'),
+        );
+      case 'deleteRows':
+        return ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            // Foreground color
+            onPrimary: Theme.of(context).colorScheme.onSecondaryContainer,
+            // Background color
+            primary: Theme.of(context).colorScheme.secondaryContainer,
+          ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
+          onPressed: null,
+          child: const Text('Delete Selected'),
+        );
+      case 'cancelChanges':
+        return ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            // Foreground color
+            onPrimary: Theme.of(context).colorScheme.onSecondaryContainer,
+            // Background color
+            primary: Theme.of(context).colorScheme.secondaryContainer,
+          ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
+          onPressed: null,
+          child: const Text('Cancel'),
+        );
+      default:
+        throw Exception(
+            'ERROR: Invalid program configuration: Unknown DataTable action: $actionKey');
+    }
+  }
+
   void _sortTable(int columnIndex, bool ascending) {
     //* TODO
+    print('_sortTable called with columnIndex: $columnIndex, asc? $ascending');
   }
 
   @override
   Widget build(BuildContext context) {
-    return _buildJetsDataTableWithScrollbars(context);
+    // return _buildJetsDataTableWithScrollbars2(context);
+    return _buildJetsDataTable(context);
   }
 
   Widget _buildJetsDataTableWithScrollbars(BuildContext context) {
@@ -71,12 +148,66 @@ class _JetsDataTableState extends State<JetsDataTableWidget> {
                 scrollDirection: Axis.horizontal,
                 controller: _horizontalController,
                 padding: const EdgeInsets.all(defaultPadding),
-                child: _buildJetsDataTable(context)),
+                child: PaginatedDataTable(
+                  header: Text(tableConfig!.title),
+                  actions:
+                      tableConfig!.actions.map((e) => _makeActions(e)).toList(),
+                  // controller: _verticalController,
+                  columns: dataColumns,
+                  sortColumnIndex: sortColumnIndex,
+                  sortAscending: sortAscending,
+                  onPageChanged: (value) =>
+                      print("onPageChange with value $value"),
+                  rowsPerPage: rowsPerPage,
+                  onRowsPerPageChanged: (value) => setState(() {
+                    rowsPerPage = value ?? rowsPerPage;
+                  }),
+                  source: dataSource!,
+                )),
           )),
     );
   }
 
+  Widget _buildJetsDataTableWithScrollbars2(BuildContext context) {
+    return Scrollbar(
+      thumbVisibility: true,
+      trackVisibility: true,
+      controller: _verticalController,
+      child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          controller: _verticalController,
+          child: Scrollbar(
+            thumbVisibility: true,
+            trackVisibility: true,
+            controller: _horizontalController,
+            child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                controller: _horizontalController,
+                padding: const EdgeInsets.all(defaultPadding),
+                child: _buildJetsDataTable2(context)),
+          )),
+    );
+  }
+
+  // Main widget
   Widget _buildJetsDataTable(BuildContext context) {
+    return PaginatedDataTable(
+      header: Text(tableConfig!.title),
+      actions: tableConfig!.actions.map((e) => _makeActions(e)).toList(),
+      columns: dataColumns,
+      sortColumnIndex: sortColumnIndex,
+      sortAscending: sortAscending,
+      onPageChanged: (value) => print("onPageChange with value $value"),
+      rowsPerPage: rowsPerPage,
+      onRowsPerPageChanged: (value) => setState(() {
+        rowsPerPage = value ?? rowsPerPage;
+      }),
+      source: dataSource!,
+    );
+  }
+
+//TEST
+  Widget _buildJetsDataTable2(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -111,12 +242,12 @@ class _JetsDataTableState extends State<JetsDataTableWidget> {
         ),
         DataTable(
           columns: List<DataColumn>.generate(
-              numItems,
+              10,
               (int index) => DataColumn(
                     label: Text('Item $index'),
                   )),
           rows: List<DataRow>.generate(
-            numItems,
+            10,
             (int index) => DataRow.byIndex(
               index: index,
               color: MaterialStateProperty.resolveWith<Color?>(
@@ -135,17 +266,9 @@ class _JetsDataTableState extends State<JetsDataTableWidget> {
                 return null; // Use default value for other states and odd rows.
               }),
               cells: List<DataCell>.generate(
-                  numItems,
+                  10,
                   (int colIndex) =>
                       DataCell(Text('Cell row $index, col $colIndex'))),
-              selected: selectedRows[index],
-              onSelectChanged: isTableEditable
-                  ? (bool? value) {
-                      setState(() {
-                        selectedRows[index] = value!;
-                      });
-                    }
-                  : null,
             ),
           ),
         ),
@@ -165,6 +288,61 @@ class _JetsDataTableState extends State<JetsDataTableWidget> {
           ),
         ],
       ),
+    );
+  }
+}
+
+typedef JetsDataRow = Map<String, dynamic>;
+typedef JetsDataModel = List<JetsDataRow>;
+
+class _JetsDataTableSource extends DataTableSource {
+  _JetsDataTableSource(this.state, this.httpClient)
+      : selectedRows = List<bool>.filled(state.tableConfig!.rowsPerPage, false);
+  final _JetsDataTableState state;
+  final HttpClient httpClient;
+  JetsDataModel? model;
+  final List<bool> selectedRows;
+  int _selectedRowCount = 0;
+
+  @override
+  int get rowCount => model != null ? model!.length : 0;
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get selectedRowCount => _selectedRowCount;
+
+  @override
+  DataRow? getRow(int index) {
+    print("JetsDataTableSource.getRow called with index $index");
+    if (model == null || model!.length >= index) return null;
+    return DataRow.byIndex(
+      index: index,
+      color: MaterialStateProperty.resolveWith<Color?>(
+          (Set<MaterialState> states) {
+        // All rows will have the same selected color.
+        if (states.contains(MaterialState.selected)) {
+          return Theme.of(state.context).colorScheme.primary.withOpacity(0.08);
+        }
+        // Even rows will have a grey color.
+        if (index.isEven) {
+          return Colors.grey.withOpacity(0.3);
+        }
+        return null; // Use default value for other states and odd rows.
+      }),
+      cells: List<DataCell>.generate(rowCount,
+          (int colIndex) => DataCell(Text('Cell row $index, col $colIndex'))),
+      selected: selectedRows[index],
+      onSelectChanged: state.isTableEditable
+          ? (bool? value) {
+              if (value == null) return;
+              if (value && !selectedRows[index]) _selectedRowCount++;
+              if (!value && selectedRows[index]) _selectedRowCount--;
+              selectedRows[index] = value;
+              notifyListeners();
+            }
+          : null,
     );
   }
 }
--- jetsclient/lib/screens/job_list.dart
+++ jetsclient/lib/screens/job_list.dart
@@ -28,67 +28,69 @@ class _JobListScreenState extends State<JobListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar('JetStore Workspace', context),
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Flexible(
-            flex: 1,
-            fit: FlexFit.tight,
-            child: Column(children: [
-              const SizedBox(height: defaultPadding),
-              Expanded(
-                flex: 1,
-                child: Image.asset('assets/images/logo.png'),
-              ),
-              const SizedBox(height: defaultPadding),
-              Expanded(
-                  flex: 8,
-                  child: ListView.separated(
-                    padding: const EdgeInsets.all(defaultPadding),
-                    itemCount: menuEntries.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          // Foreground color
-                          onPrimary: Theme.of(context)
-                              .colorScheme
-                              .onSecondaryContainer,
-                          // Background color
-                          primary:
-                              Theme.of(context).colorScheme.secondaryContainer,
-                        ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
-                        onPressed: menuActions[index],
-                        child: Center(child: Text(menuEntries[index])),
-                      );
-                    },
-                    separatorBuilder: (BuildContext context, int index) =>
-                        const Divider(),
-                  ))
-            ]),
-          ),
-          Flexible(
-            flex: 5,
-            fit: FlexFit.tight,
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              const SizedBox(height: 2 * defaultPadding),
-              Flexible(
-                flex: 1,
-                fit: FlexFit.tight,
-                child: Text(
-                  'Data Pipelines',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-              ),
-              const Flexible (
-                flex: 8,
-                fit: FlexFit.tight,
-                child: JetsDataTableWidget(tableConfig: "joblist"),
-              ),
-            ]),
-          ),
-        ],
-      ),
+      body: 
+      const JetsDataTableWidget(tableConfig: "joblist"),
+      // Row(
+      //   crossAxisAlignment: CrossAxisAlignment.start,
+      //   children: [
+      //     Flexible(
+      //       flex: 1,
+      //       fit: FlexFit.tight,
+      //       child: Column(children: [
+      //         const SizedBox(height: defaultPadding),
+      //         Expanded(
+      //           flex: 1,
+      //           child: Image.asset('assets/images/logo.png'),
+      //         ),
+      //         const SizedBox(height: defaultPadding),
+      //         Expanded(
+      //             flex: 8,
+      //             child: ListView.separated(
+      //               padding: const EdgeInsets.all(defaultPadding),
+      //               itemCount: menuEntries.length,
+      //               itemBuilder: (BuildContext context, int index) {
+      //                 return ElevatedButton(
+      //                   style: ElevatedButton.styleFrom(
+      //                     // Foreground color
+      //                     onPrimary: Theme.of(context)
+      //                         .colorScheme
+      //                         .onSecondaryContainer,
+      //                     // Background color
+      //                     primary:
+      //                         Theme.of(context).colorScheme.secondaryContainer,
+      //                   ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
+      //                   onPressed: menuActions[index],
+      //                   child: Center(child: Text(menuEntries[index])),
+      //                 );
+      //               },
+      //               separatorBuilder: (BuildContext context, int index) =>
+      //                   const Divider(),
+      //             ))
+      //       ]),
+      //     ),
+      //     Flexible(
+      //       flex: 5,
+      //       fit: FlexFit.tight,
+      //       child:
+      //           Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      //         const SizedBox(height: 2 * defaultPadding),
+      //         Flexible(
+      //           flex: 1,
+      //           fit: FlexFit.tight,
+      //           child: Text(
+      //             'Data Pipelines',
+      //             style: Theme.of(context).textTheme.headline4,
+      //           ),
+      //         ),
+      //         const Flexible (
+      //           flex: 8,
+      //           fit: FlexFit.tight,
+      //           child: JetsDataTableWidget(tableConfig: "joblist"),
+      //         ),
+      //       ]),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
