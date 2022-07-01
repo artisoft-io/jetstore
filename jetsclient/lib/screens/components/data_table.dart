a888c7c5e530f9b538951f4d246cb5ea1d6bafc0
Michel Dufresne <3496090+cpp-cat@users.noreply.github.com>
2022-06-30 20:06:08 -0400

    JetStore Client: Define a JetsDataTable widget #314

--- jetsclient/lib/screens/components/data_table.dart
+++ jetsclient/lib/screens/components/data_table.dart
@@ -28,11 +28,13 @@ class JetsDataTableState extends State<JetsDataTableWidget> {
   bool isTableEditable = false;
   int sortColumnIndex = 0;
   bool sortAscending = false;
+
+  // pagination state
   int currentDataPage = 0;
   int rowsPerPage = 10;
+  late final List<int> availableRowsPerPage;
+
   late final List<DataColumn> dataColumns;
-  late final List<Widget> actionWhenTableEditable;
-  late final List<Widget> actionWhenTableNotEditable;
 
   int get indexOffset => currentDataPage * rowsPerPage;
   int get maxIndex => (currentDataPage + 1) * rowsPerPage;
@@ -44,6 +46,12 @@ class JetsDataTableState extends State<JetsDataTableWidget> {
     sortColumnIndex = tableConfig.sortColumnIndex;
     sortAscending = tableConfig.sortAscending;
     rowsPerPage = tableConfig.rowsPerPage;
+    availableRowsPerPage = <int>[
+      rowsPerPage,
+      rowsPerPage * 2,
+      rowsPerPage * 5,
+      rowsPerPage * 10
+    ];
 
     dataColumns = tableConfig.columns
         .map((e) => DataColumn(
@@ -53,165 +61,255 @@ class JetsDataTableState extends State<JetsDataTableWidget> {
             onSort: ((columnIndex, ascending) =>
                 _sortTable(columnIndex, ascending))))
         .toList();
-    actionWhenTableEditable = makeActionWidgets(true);
-    actionWhenTableNotEditable = makeActionWidgets(false);
 
     dataSource = JetsDataTableSource(
         this, Provider.of<HttpClient>(context, listen: false));
+    dataSource.addListener(() {
+      setState(() {});
+    });
     // Get the first batch of data
     dataSource.getModelDataSync();
   }
 
   @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  @override
   void dispose() {
+    dataSource.removeListener(() {
+      setState(() {});
+    });
     dataSource.dispose();
     super.dispose();
   }
 
-  List<Widget> makeActionWidgets(bool isTblEditable) {
-    return tableConfig.actions
-        .where((ac) => ac.predicate(isTblEditable))
-        .map((ac) => ElevatedButton(
-              style: ac.buttonStyle(Theme.of(context)),
-              onPressed: () => actionDispatcher(ac),
-              child: Text(ac.label),
-            ))
-        .toList();
-  }
-
   void actionDispatcher(ActionConfig ac) {
     //* TODO
+    switch (ac.key) {
+      case 'new':
+        showAlertDialog(context, 'New Pipeline Coming Soon!');
+        break;
+      case 'edit':
+        setState(() => isTableEditable = true);
+        break;
+      case 'save':
+        showAlertDialog(context, 'Save Changes Coming Soon!');
+        setState(() => isTableEditable = false);
+        break;
+      case 'delete':
+        showAlertDialog(
+            context, 'Delete Pipeline (with confirmation) Coming Soon!');
+        setState(() => isTableEditable = false);
+        break;
+      case 'cancel':
+        showAlertDialog(
+            context, 'Cancel changes (with confirmation) Coming Soon!');
+        setState(() => isTableEditable = false);
+        break;
+
+      default:
+        showAlertDialog(
+            context, 'Oops something is wrong, unknown action ${ac.key}');
+    }
   }
 
   void _sortTable(int columnIndex, bool ascending) {
-    //* TODO
-    print('_sortTable called with columnIndex: $columnIndex, asc? $ascending');
+    dataSource.sortModelData(columnIndex, ascending);
+    setState(() {
+      sortColumnIndex = columnIndex;
+      sortAscending = ascending;
+    });
+  }
+
+  // Functions for pagination
+  bool _isLastPage() {
+    return (currentDataPage + 1) * rowsPerPage >= dataSource.totalRowCount;
+  }
+
+  void _rowPerPageChanged(int? value) {
+    if (value == null) return;
+    setState(() {
+      rowsPerPage = value;
+    });
+    //* TODO dot it asynch
+    dataSource.getModelDataSync();
+  }
+
+  void _gotoFirstPressed() {
+    setState(() {
+      currentDataPage = 0;
+    });
+    //* TODO dot it asynch
+    dataSource.getModelDataSync();
+  }
+
+  void _previousPressed() {
+    setState(() {
+      currentDataPage--;
+    });
+    //* TODO dot it asynch
+    dataSource.getModelDataSync();
+  }
+
+  void _nextPressed() {
+    setState(() {
+      currentDataPage++;
+    });
+    //* TODO dot it asynch
+    dataSource.getModelDataSync();
+  }
+
+  void _lastPressed() {
+    var n = dataSource.totalRowCount ~/ rowsPerPage;
+    var r = dataSource.totalRowCount % n;
+    setState(() {
+      currentDataPage = r == 0 ? n - 1 : n;
+    });
+    //* TODO dot it asynch
+    dataSource.getModelDataSync();
   }
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    final MaterialLocalizations localizations =
+        MaterialLocalizations.of(context);
+    // prepare the footer widgets
+    final TextStyle? footerTextStyle = themeData.textTheme.caption;
+    List<DropdownMenuItem<int>> rowsPerPageItems = availableRowsPerPage
+        .map<DropdownMenuItem<int>>((e) => DropdownMenuItem<int>(
+              value: e,
+              child: Text('$e'),
+            ))
+        .toList();
+    var footerWidgets = <Widget>[
+      Container(
+          width:
+              14.0), // to match trailing padding in case we overflow and end up scrolling
+      Text(localizations.rowsPerPageTitle),
+      ConstrainedBox(
+        constraints: const BoxConstraints(
+            minWidth: 64.0), // 40.0 for the text, 24.0 for the icon
+        child: Align(
+          alignment: AlignmentDirectional.centerEnd,
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton<int>(
+              items: rowsPerPageItems,
+              value: rowsPerPage,
+              onChanged: _rowPerPageChanged,
+              style: footerTextStyle,
+            ),
+          ),
+        ),
+      ),
+      Container(width: 32.0),
+      Text(
+        localizations.pageRowsInfoTitle(
+          indexOffset + 1,
+          maxIndex + 1,
+          dataSource.totalRowCount,
+          false,
+        ),
+      ),
+      Container(width: 32.0),
+      IconButton(
+        icon: const Icon(Icons.skip_previous),
+        padding: EdgeInsets.zero,
+        tooltip: localizations.firstPageTooltip,
+        onPressed: currentDataPage == 0 ? null : _gotoFirstPressed,
+      ),
+      IconButton(
+        icon: const Icon(Icons.chevron_left),
+        padding: EdgeInsets.zero,
+        tooltip: localizations.previousPageTooltip,
+        onPressed: currentDataPage == 0 ? null : _previousPressed,
+      ),
+      Container(width: 24.0),
+      IconButton(
+        icon: const Icon(Icons.chevron_right),
+        padding: EdgeInsets.zero,
+        tooltip: localizations.nextPageTooltip,
+        onPressed: _isLastPage() ? null : _nextPressed,
+      ),
+      IconButton(
+        icon: const Icon(Icons.skip_next),
+        padding: EdgeInsets.zero,
+        tooltip: localizations.lastPageTooltip,
+        onPressed: _isLastPage() ? null : _lastPressed,
+      ),
+      Container(width: 14.0),
+    ];
+
+    // build the data table
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         // HEADER ROW
         Row(
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor:
-                    Theme.of(context).colorScheme.onSecondaryContainer,
-                backgroundColor:
-                    Theme.of(context).colorScheme.secondaryContainer,
-              ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
-              onPressed: () => showAlertDialog(context, 'Coming Soon!'),
-              child: const Text('New Pipeline'),
-            ),
-            const SizedBox(width: defaultPadding),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor:
-                    Theme.of(context).colorScheme.onSecondaryContainer,
-                backgroundColor:
-                    Theme.of(context).colorScheme.secondaryContainer,
-              ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
-              onPressed: () {
-                setState(() {
-                  isTableEditable = !isTableEditable;
-                });
-              },
-              child: const Text('Edit Table'),
-            ),
-          ],
+          children: tableConfig.actions
+        .where((ac) => ac.predicate(isTableEditable))
+        .expand((ac) => [
+              const SizedBox(width: defaultPadding),
+              ElevatedButton(
+                style: ac.buttonStyle(themeData),
+                onPressed: () => actionDispatcher(ac),
+                child: Text(ac.label),
+              )
+            ])
+        .toList()
+            ,
         ),
         // MAIN TABLE SECTION
         const SizedBox(height: defaultPadding),
         Expanded(
-            flex: 8,
-            child: Scrollbar(
-              thumbVisibility: true,
-              trackVisibility: true,
+          child: Scrollbar(
+          thumbVisibility: true,
+          trackVisibility: true,
+          controller: _verticalController,
+          child: Scrollbar(
+            thumbVisibility: true,
+            trackVisibility: true,
+            controller: _horizontalController,
+            notificationPredicate: (e) => e.depth == 1,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
               controller: _verticalController,
-              child: Scrollbar(
-                thumbVisibility: true,
-                trackVisibility: true,
-                controller: _horizontalController,
-                notificationPredicate: (e) => e.depth == 1,
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  controller: _verticalController,
-                  child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      controller: _horizontalController,
-                      padding: const EdgeInsets.all(defaultPadding),
-                      child: DataTable(
-                        columns: List<DataColumn>.generate(
-                            10,
-                            (int index) => DataColumn(
-                                  label: Text('Item $index'),
-                                )),
-                        rows: List<DataRow>.generate(
-                          10,
-                          (int index) => DataRow.byIndex(
-                            index: index,
-                            color: MaterialStateProperty.resolveWith<Color?>(
-                                (Set<MaterialState> states) {
-                              // All rows will have the same selected color.
-                              if (states.contains(MaterialState.selected)) {
-                                return Theme.of(context)
-                                    .colorScheme
-                                    .primary
-                                    .withOpacity(0.08);
-                              }
-                              // Even rows will have a grey color.
-                              if (index.isEven) {
-                                return Colors.grey.withOpacity(0.3);
-                              }
-                              return null; // Use default value for other states and odd rows.
-                            }),
-                            cells: List<DataCell>.generate(
-                                10,
-                                (int colIndex) => DataCell(
-                                    Text('Cell row $index, col $colIndex'))),
-                          ),
-                        ),
-                      )),
-                ),
-              ),
-            )),
+              child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  controller: _horizontalController,
+                  padding: const EdgeInsets.all(defaultPadding),
+                  child: DataTable(
+                    columns: dataColumns,
+                    rows: List<DataRow>.generate(
+                      dataSource.rowCount,
+                      (int index) => dataSource.getRow(index),
+                    ),
+                  )),
+            ),
+          ),
+        )),
         // FOOTER ROW
         const SizedBox(height: defaultPadding),
-        Expanded(
-          flex: 1,
-          child: Row(
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor:
-                      Theme.of(context).colorScheme.onSecondaryContainer,
-                  backgroundColor:
-                      Theme.of(context).colorScheme.secondaryContainer,
-                ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
-                onPressed: () => showAlertDialog(context, 'Coming Soon!'),
-                child: const Text('Footer New Pipeline'),
+        DefaultTextStyle(
+          style: footerTextStyle!,
+          child: IconTheme.merge(
+            data: const IconThemeData(
+              opacity: 0.54,
+            ),
+            child: SizedBox(
+              height: 56.0,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                reverse: true,
+                child: Row(
+                  children: footerWidgets,
+                ),
               ),
-              const SizedBox(width: defaultPadding),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor:
-                      Theme.of(context).colorScheme.onSecondaryContainer,
-                  backgroundColor:
-                      Theme.of(context).colorScheme.secondaryContainer,
-                ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
-                onPressed: () {
-                  setState(() {
-                    isTableEditable = !isTableEditable;
-                  });
-                },
-                child: const Text('Edit Table'),
-              ),
-            ],
+            ),
           ),
-        )
+        ),
       ],
     );
   }
