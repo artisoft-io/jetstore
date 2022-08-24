d3d23481d3131bb7febd47a58f06b37d38494fbc
Michel Dufresne <3496090+cpp-cat@users.noreply.github.com>
2022-08-23 22:12:39 -0400

    JetStoe api server: Add end point to register file key and start process automatically #376

---
+++ jets/apiserver/api_filekey.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+type RegisterFileKeyAction struct {
+	Action         string            			  `json:"action"`
+	ProcessName    string            			  `json:"process_name"`
+	Data           []map[string]interface{} `json:"data"`
+}
+
+// DoRegisterFileKeyAction ------------------------------------------------------
+// Entry point function
+func (server *Server) DoRegisterFileKeyAction(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	registerFileKeyAction := RegisterFileKeyAction{}
+	err = json.Unmarshal(body, &registerFileKeyAction)
+	if err != nil {
+		ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	// Intercept specific dataTable action
+	switch registerFileKeyAction.Action {
+
+	case "register_keys":
+		server.RegisterKeys(w, r, &registerFileKeyAction)
+		return
+
+	default:
+		log.Printf("Error: unknown action: %v", registerFileKeyAction.Action)
+		ERROR(w, http.StatusUnprocessableEntity, fmt.Errorf("error: unknown action"))
+		return
+	}
+}
+
+// RegisterKeys ------------------------------------------------------
+func (server *Server) RegisterKeys(w http.ResponseWriter, r *http.Request, registerFileKeyAction *RegisterFileKeyAction) {
+	// RegisterFileKeyAction.ProcessName if not empty, a pipeline execution will be started
+	// Expected input keys for each register key entry (see sql_stmts.go):
+	//	"client", "object_type", "file_key"
+	sqlStmt, ok := sqlInsertStmts["file_key_staging"]
+	if !ok {
+		ERROR(w, http.StatusBadRequest, errors.New("error cannot find file_key_staging stmt"))
+		return
+	}
+	row := make([]interface{}, len(sqlStmt.columnKeys))
+	for irow := range registerFileKeyAction.Data {
+		for jcol, colKey := range sqlStmt.columnKeys {
+			row[jcol] = registerFileKeyAction.Data[irow][colKey]
+		}
+		// fmt.Printf("Insert Row for stmt on table %s: %v\n", registerFileKeyAction.Table, row)
+		_, err := server.dbpool.Exec(context.Background(), sqlStmt.stmt, row...)
+		if err != nil {
+			log.Printf("while inserting on table file_key_staging: %v", err)
+			ERROR(w, http.StatusInternalServerError, errors.New("error while inserting on table file_key_staging"))	
+			return
+		}
+		if len(registerFileKeyAction.ProcessName) > 0 {
+			// Start the process, currently supporting only load+start process
+			processName := registerFileKeyAction.ProcessName
+			client := registerFileKeyAction.Data[irow]["client"]
+			objectType := registerFileKeyAction.Data[irow]["object_type"]
+			fileKey := registerFileKeyAction.Data[irow]["file_key"]
+			sessionId := strconv.FormatInt(time.Now().UnixMilli(), 10)
+			var pipelineConfigKey int
+			stmt := "SELECT key FROM jetsapi.pipeline_config WHERE process_name=$1 AND client=$2 AND main_object_type=$3"
+			err := server.dbpool.QueryRow(context.Background(), stmt, processName, client, objectType).Scan(&pipelineConfigKey)
+			if err != nil {
+				log.Printf("in RegisterKeys while querying pipeline_config to start a process: %v", err)
+				ERROR(w, http.StatusInternalServerError, errors.New("error while fetching pipeline_config key"))	
+				return
+			}
+			dataTableAction := DataTableAction {
+				Action: "insert_rows", 
+				Table: "short/pipeline_execution_status",
+				Data: []map[string]interface{}{
+					{"pipeline_config_key":  strconv.Itoa(pipelineConfigKey),
+					"load_and_start": "true",
+					// "main_input_registry_key": nil,
+					"main_input_file_key": fileKey,
+					"file_key": fileKey,
+					"table_name": fmt.Sprintf("%s_%s", client, objectType),
+					// "merged_input_registry_keys": "'{33}'",
+					"client": client,
+					"process_name": processName,
+					"main_object_type": objectType,
+					"object_type": objectType,
+					"input_session_id": sessionId,
+					"session_id": sessionId,
+					"status": "submitted",
+					"user_email": "system"},
+			}}
+			_, httpStatus, err := server.ProcessInsertRows(&dataTableAction)
+			if(httpStatus != http.StatusOK) {
+				ERROR(w, httpStatus, err)	
+				return
+			}
+		}
+	}
+
+	results := makeResult(r)
+	JSON(w, http.StatusOK, results)
+}
