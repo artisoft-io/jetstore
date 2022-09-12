2b9ce1192495cea52d4a9a896c9555e0c5491eb5
Michel Dufresne <3496090+cpp-cat@users.noreply.github.com>
2022-09-12 13:46:40 -0400

    JetStore Client: Add Data Purge capability to clear the db of all processing data (residing in the domain tables) Fixes #401

---
+++ jetsclient/lib/screens/components/menu_delegates/menu_delegates.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:jetsclient/routes/export_routes.dart';
+import 'package:provider/provider.dart';
+import 'package:jetsclient/http_client.dart';
+import 'package:jetsclient/screens/components/dialogs.dart';
+
+/// Menu Delegates are action functions to perform on the menu button
+/// onPress method without navigating to a new form, typically use
+/// a simple dialog for confirmation and issue a command to back end
+void purgeDataAction(BuildContext context) async {
+  var client = context.read<HttpClient>();
+  var messenger = ScaffoldMessenger.of(context);
+  // get user confirmation
+  var uc = await showDangerZoneDialog(context,
+      'Are you sure you want to purge ALL client data and rebuild the client tables?');
+  // print('purgeData Action GOT: $uc');
+  if (uc != 'OK') return;
+  var encodedJsonBody = jsonEncode(<String, dynamic>{
+    'action': 'reset_domain_tables',
+    'table': '',
+    'data': [{}],
+  }, toEncodable: (_) => '');
+  var result = await client.sendRequest(
+      path: purgeDataPath, 
+      token: JetsRouterDelegate().user.token,
+      encodedJsonBody: encodedJsonBody);
+  // if (!mounted) return; needed?
+  if (result.statusCode == 200) {
+    // Inform the user and transition
+    const snackBar = SnackBar(
+      content: Text('Purge Client Data Successful'),
+    );
+    messenger.showSnackBar(snackBar);
+  } else {
+    showAlertDialog(context, 'Something went wrong. Please try again.');
+  }
+}
