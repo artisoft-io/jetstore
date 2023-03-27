a76d3fe8be6b04ad7334aaba165b4f7b45ec7187
Michel Dufresne <3496090+cpp-cat@users.noreply.github.com>
2023-03-27 11:55:18 -0400

    Added AssetManifest.bin to file list for ui build

--- jets/apiserver/server.go
+++ jets/apiserver/server.go
@@ -306,6 +306,7 @@ func listenAndServe() error {
 	server.Router.Handle("/assets/NOTICES", fs).Methods("GET")
 	server.Router.Handle("/assets/fonts/MaterialIcons-Regular.otf", fs).Methods("GET")
 	server.Router.Handle("/assets/AssetManifest.json", fs).Methods("GET")
+	server.Router.Handle("/assets/AssetManifest.bin", fs).Methods("GET")
 	server.Router.Handle("/assets/assets/fonts/RobotoCondensed-Bold.ttf", fs).Methods("GET")
 	server.Router.Handle("/assets/assets/fonts/RobotoCondensed-Italic.ttf", fs).Methods("GET")
 	server.Router.Handle("/assets/assets/fonts/Roboto-Regular.ttf", fs).Methods("GET")
