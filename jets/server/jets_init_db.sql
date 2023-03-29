e8ab2ab31f7c7ca1ff21ef5bbfdd6536b5a0ef42
Michel Dufresne <3496090+cpp-cat@users.noreply.github.com>
2023-03-28 20:18:49 -0400

    JetStore Add trim function to clean input data #657

--- jets/server/jets_init_db.sql
+++ jets/server/jets_init_db.sql
@@ -1,6 +1,7 @@
 
 TRUNCATE jetsapi.mapping_function_registry;
 INSERT INTO jetsapi.mapping_function_registry (function_name, is_argument_required) VALUES
+  ('trim', '0'),
   ('to_upper', '0'),
   ('parse_amount', '1'),
   ('reformat0', '1'),
