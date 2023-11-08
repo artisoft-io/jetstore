25e276d65d83f8e9a0ab56c1cfa735d68f9d0055
Michel Dufresne <3496090+cpp-cat@users.noreply.github.com>
2023-11-07 22:22:53 -0500

    Added github ip to securitygroup of ui

--- cdk/jetstore_one/jetstore_one.go
+++ cdk/jetstore_one/jetstore_one.go
@@ -1143,6 +1143,45 @@ func NewJetstoreOneStack(scope constructs.Construct, id string, props *JetstoreO
 			StreamPrefix: jsii.String("task"),
 		}),
 	})
+	securityGroup := awsec2.NewSecurityGroup(stack, jsii.String("UI-SecurityGroup"), &awsec2.SecurityGroupProps{
+		Vpc: vpc,
+		Description: jsii.String("Allow JetStore UI network access"),
+		AllowAllOutbound: jsii.Bool(false),
+		DisableInlineRules: jsii.Bool(true),
+	})
+	// Add access to Github
+	cdrs := jsii.Strings(    
+		"192.30.252.0/22",
+		"185.199.108.0/22",
+		"140.82.112.0/20",
+		"143.55.64.0/20",
+		"2a0a:a440::/29",
+		"2606:50c0::/32",
+		"20.201.28.151/32",
+		"20.205.243.166/32",
+		"20.87.245.0/32",
+		"20.248.137.48/32",
+		"20.207.73.82/32",
+		"20.27.177.113/32",
+		"20.200.245.247/32",
+		"20.175.192.147/32",
+		"20.233.83.145/32",
+		"20.29.134.23/32",
+		"20.201.28.152/32",
+		"20.205.243.160/32",
+		"20.87.245.4/32",
+		"20.248.137.50/32",
+		"20.207.73.83/32",
+		"20.27.177.118/32",
+		"20.200.245.248/32",
+		"20.175.192.146/32",
+		"20.233.83.149/32",
+		"20.29.134.19/32",
+	)
+	for _,cdr := range *cdrs {
+		securityGroup.AddEgressRule(awsec2.Peer_Ipv4(cdr), awsec2.Port_Tcp(jsii.Number(443)), 
+			jsii.String("allow https access to github repository"), jsii.Bool(false))
+	}	
 	ecsUiService := awsecs.NewFargateService(stack, jsii.String("jetstore-ui"), &awsecs.FargateServiceProps{
 		Cluster:        ecsCluster,
 		ServiceName:    jsii.String("jetstore-ui"),
@@ -1150,6 +1189,7 @@ func NewJetstoreOneStack(scope constructs.Construct, id string, props *JetstoreO
 		VpcSubnets:     privateSubnetSelection,
 		AssignPublicIp: jsii.Bool(false),
 		DesiredCount:   jsii.Number(1),
+		SecurityGroups: &[]awsec2.ISecurityGroup{securityGroup},
 	})
 	if phiTagName != nil {
 		awscdk.Tags_Of(ecsUiService).Add(phiTagName, jsii.String("true"), nil)
