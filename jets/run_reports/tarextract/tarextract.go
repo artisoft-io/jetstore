c334c86d1148eb0f6bd42e2ecaeaf5d6264f19e1
Michel Dufresne <3496090+cpp-cat@users.noreply.github.com>
2024-04-27 00:57:07 -0400

    add tarextract package

---
+++ jets/run_reports/tarextract/tarextract.go
@@ -0,0 +1,57 @@
+package tarextract
+
+// hat tip https://gist.github.com/indraniel/1a91458984179ab4cf80
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func ExtractTarGz(gzipStream io.Reader) error {
+	uncompressedStream, err := gzip.NewReader(gzipStream)
+	if err != nil {
+		log.Fatal("ExtractTarGz: NewReader failed")
+	}
+
+	tarReader := tar.NewReader(uncompressedStream)
+
+	for {
+		header, err := tarReader.Next()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("ExtractTarGz: Next() failed: %s", err.Error())
+		}
+
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if err := os.Mkdir(header.Name, 0755); err != nil {
+				log.Fatalf("ExtractTarGz: Mkdir() failed: %s", err.Error())
+			}
+		case tar.TypeReg:
+			outFile, err := os.Create(header.Name)
+			if err != nil {
+				log.Fatalf("ExtractTarGz: Create() failed: %s", err.Error())
+			}
+			defer outFile.Close()
+			if _, err := io.Copy(outFile, tarReader); err != nil {
+				log.Fatalf("ExtractTarGz: Copy() failed: %s", err.Error())
+			}
+		default:
+			err := fmt.Errorf(
+				"ExtractTarGz: uknown type: %v in %s",
+				header.Typeflag,
+				header.Name)
+			log.Println(err)
+			return err
+		}
+	}
+	return nil
+}
