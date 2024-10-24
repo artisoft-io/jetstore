dfdae8074a8186013faf7482bf1cf046e91c7509
Michel Dufresne <3496090+cpp-cat@users.noreply.github.com>
2024-10-24 16:38:41 -0400

    Add unique_split_on cleansing function to split an input value and append a suffix -n #1476

---
+++ jets/cleansing_functions/cleansing_functions_test.go
@@ -0,0 +1,116 @@
+package cleansing_functions
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSplitOn(t *testing.T) {
+	inputValue := ""
+	argument := ","
+	obj := SplitOn(&inputValue, &argument)
+	if obj != nil {
+		t.Errorf("error: expecting nil")
+	}
+	// another test
+	inputValue = "value1,value2,value3"
+	argument = ","
+	obj = SplitOn(&inputValue, &argument)
+	objV := obj.([]string)
+	fmt.Println("objV=",objV)
+	if len(objV) != 3 {
+		t.Errorf("error: expecting 3 values")
+	}
+	m := make(map[string]bool)
+	for _,v := range objV {
+		m[v] = true
+	}
+	if !m["value1"] {
+		t.Errorf("error: missing expected value")
+	}
+	if !m["value2"] {
+		t.Errorf("error: missing expected value")
+	}
+	if !m["value3"] {
+		t.Errorf("error: missing expected value")
+	}
+	// another test
+	inputValue = "value1,value2,value1"
+	argument = ","
+	obj = SplitOn(&inputValue, &argument)
+	objV = obj.([]string)
+	fmt.Println("objV=",objV)
+	if len(objV) != 3 {
+		t.Errorf("error: expecting 3 values")
+	}
+	m = make(map[string]bool)
+	for _,v := range objV {
+		m[v] = true
+	}
+	if len(m) != 2 {
+		t.Errorf("error: expecting 2 unique values")
+	}
+	if !m["value1"] {
+		t.Errorf("error: missing expected value")
+	}
+	if !m["value2"] {
+		t.Errorf("error: missing expected value")
+	}
+}
+
+func TestUniqueSplitOn(t *testing.T) {
+	inputValue := ""
+	argument := ","
+	obj := UniqueSplitOn(&inputValue, &argument)
+	fmt.Println("obj=",obj)
+	if obj != nil {
+		t.Errorf("error: expecting nil")
+	}
+	// another test
+	inputValue = "value1,value2,value3"
+	argument = ","
+	obj = UniqueSplitOn(&inputValue, &argument)
+	objV := obj.([]string)
+	fmt.Println("objV=",objV)
+	if len(objV) != 3 {
+		t.Errorf("error: expecting 3 values")
+	}
+	m := make(map[string]bool)
+	for _,v := range objV {
+		m[v] = true
+	}
+	if len(m) != 3 {
+		t.Errorf("error: expecting 3 unique values")
+	}
+	if !m["value1-0"] {
+		t.Errorf("error: missing expected value")
+	}
+	if !m["value2-0"] {
+		t.Errorf("error: missing expected value")
+	}
+	if !m["value3-0"] {
+		t.Errorf("error: missing expected value")
+	}
+	// another test
+	inputValue = "value1,value2,value1"
+	argument = ","
+	obj = UniqueSplitOn(&inputValue, &argument)
+	objV = obj.([]string)
+	fmt.Println("objV=",objV)
+	if len(objV) != 3 {
+		t.Errorf("error: expecting 3 values")
+	}
+	m = make(map[string]bool)
+	for _,v := range objV {
+		m[v] = true
+	}
+	if !m["value1-0"] {
+		t.Errorf("error: missing expected value")
+	}
+	if !m["value2-0"] {
+		t.Errorf("error: missing expected value")
+	}
+	if !m["value1-1"] {
+		t.Errorf("error: missing expected value")
+	}
+}
