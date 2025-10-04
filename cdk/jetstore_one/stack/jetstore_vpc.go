71aad10f1b1f6e139cddfcff6751838de3e100d6
Michel Dufresne <3496090+cpp-cat@users.noreply.github.com>
2025-10-04 16:50:33 -0400

    Add log on egress rules

--- cdk/jetstore_one/stack/jetstore_vpc.go
+++ cdk/jetstore_one/stack/jetstore_vpc.go
@@ -77,6 +77,8 @@ func LookupVpcEndpointsSecurityGroup(stack awscdk.Stack, sgId string) awsec2.ISe
 	if sg == nil {
 		log.Fatal("Failed to lookup security group, please check JETS_VPC_ENDPOINTS_SG_ID")
 	}
+	log.Printf("Resolved VPC Endpoints Security Group '%s'\n", *sg.SecurityGroupId())
+	log.Printf("Egress rules are %T, %v '%s'\n", sg.ToEgressRuleConfig(), sg.ToEgressRuleConfig(), sg.ToEgressRuleConfig())
 	return sg
 }
 
